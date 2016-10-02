cd73e508c647c6b68e2d6a1e83fcf4cd8f7c3666
Taylor Johnson <taylor.c.johnson@usace.army.mil>
2016-10-02 17:29:01 -0400

    Map fetchPosts action dispatch to PostsIndex container

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts</div>;
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchPosts }, dispatch);
+// }
+
+//export default connect(null, mapDispatchToProps)(PostsIndex);
+export default connect(null, { fetchPosts: fetchPosts })(PostsIndex); // refactor replaces mapDispatchToProps function
+export default connect(null, { fetchPosts })(PostsIndex); // refactor again with ES6 syntax
