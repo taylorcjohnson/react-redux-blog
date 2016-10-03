34ef1f36939a8e030abb1039bdcec1dcc88d8507
Taylor Johnson <taylor.c.johnson@usace.army.mil>
2016-10-03 16:11:45 -0400

    List posts on index page and add route and link to post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -9,6 +9,19 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={`/posts/${post.id}`}>
+            <span className="pull-xs-right">{post.categories}</span>
+            <strong>{post.title}</strong>
+          </Link>
+        </li>
+      );
+    })
+  }
+
   render() {
     return (
       <div>
@@ -17,10 +30,17 @@ class PostsIndex extends Component {
             Add a post
           </Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
 }
 
 // function mapDispatchToProps(dispatch) {
@@ -29,4 +49,4 @@ class PostsIndex extends Component {
 
 //export default connect(null, mapDispatchToProps)(PostsIndex);
 //export default connect(null, { fetchPosts: fetchPosts })(PostsIndex); // refactor replaces mapDispatchToProps function
-export default connect(null, { fetchPosts })(PostsIndex); // refactor again with ES6 syntax
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex); // refactor again with ES6 syntax
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // createPost returns a promise, if/when it is resolved that means the post was successfully made
+        // now, navigate user back to index
+        this.context.router.push("/");
+      });
+  }
+
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create A New Post</h3>
 
         <div className={ `form-group ${title.touched && title.invalid ? 'has-danger' : ''}` }>
